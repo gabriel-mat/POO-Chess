5c96a16b681a6f442e7bc04fe97f5c1ded50f09d
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-06-17 19:56:46 -0300

    Adição da peça bispo

---
+++ chess/src/pecas/Bispo.java
@@ -0,0 +1,46 @@
+package pecas;
+
+import java.lang.Math;
+
+public class Bispo extends Peca{
+    public Bispo (String cor) {
+        super(cor);
+    }
+
+    public char getTipo(){
+        return 'B';
+    }
+
+    public String desenho(){
+        return "B";
+    }
+
+    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD){
+        return (Math.abs(linhaD - linhaO) == Math.abs(colunaD - colunaO));
+    }
+
+    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD){
+        String retorno = "";
+
+        if(!movimentoValido(linhaO, colunaO, linhaD, colunaD))
+            return retorno;
+
+        if(linhaD > linhaO){
+            if(colunaD > colunaO)
+                for(int i = linhaO; i <= linhaD; i++)
+                    retorno += i + colunaO++;
+            else
+                for(int i = linhaO; i <= linhaD; i++)
+                    retorno += i + colunaO--;
+        }else{
+            if(colunaD > colunaO)
+                for(int i = linhaO; i >= linhaD; i--)
+                    retorno += i + colunaO++;
+            else
+                for(int i = linhaO; i >= linhaD; i--)
+                    retorno += i + colunaO--;
+        }
+
+        return retorno;
+    }
+}
