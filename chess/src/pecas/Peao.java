f134c6500fe825d7ad0b12ca9b2e243e64d3085e
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-06-17 14:55:28 -0300

    Base de peças implementada

---
+++ chess/src/pecas/Peao.java
@@ -0,0 +1,26 @@
+package pecas;
+
+public class Peao extends Peca{
+    public Peao(String cor){
+        super(cor);
+    }
+
+    public String desenho(){
+        return "P";
+    }
+
+    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD){
+        if(getCor().equals("Branco"))
+            return ((colunaO == colunaD) && (linhaD == linhaO + 1));
+        return ((colunaO == colunaD) && (linhaD == linhaO - 1));
+    }
+
+    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD){
+        String retorno = "";
+
+        if(!movimentoValido(linhaO, colunaO, linhaD, colunaD))
+            return retorno;
+
+        return retorno + linhaO + colunaO + linhaD + colunaD;
+    }
+}
---
+++ chess/src/pecas/Peca.java
@@ -0,0 +1,17 @@
+package pecas;
+
+public abstract class Peca {
+    private String cor;
+
+    public Peca(String cor){
+        this.cor = cor;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+
+    public abstract String desenho();
+    public abstract boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD);
+    public abstract String caminho(int linhaO, char colunaO, int linhaD, char colunaD);
+}
