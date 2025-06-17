b5051ede99f468c8b1c5e0220fc6cf9f5e89a214
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-06-17 18:34:31 -0300

    Adição da peça torre

---
+++ chess/src/pecas/Torre.java
@@ -0,0 +1,41 @@
+package pecas;
+
+public class Torre extends Peca{
+    public Torre(String cor){
+        super(cor);
+    }
+
+    public String desenho(){
+        return "R";
+    }
+
+    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD){
+        return ((linhaO == linhaD) || (colunaO == colunaD));
+    }
+
+    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD){
+        String retorno = "";
+
+        if(!movimentoValido(linhaO, colunaO, linhaD, colunaD))
+            return retorno;
+
+        if(linhaO != linhaD){
+            if(linhaO < linhaD)
+                for(int i = linhaO; i <= linhaD; i++)
+                    retorno += i + colunaO;
+            else
+                for(int i = linhaO; i >= linhaD; i--)
+                    retorno += i + colunaO;
+        }
+        else{
+            if(colunaO < colunaD)
+                for(char i = colunaO; i <= colunaD; i++)
+                    retorno += linhaO + i;
+            else
+                for(char i = colunaO; i >= colunaD; i--)
+                    retorno += linhaO + i;
+        }
+
+        return retorno;
+    }
+}
