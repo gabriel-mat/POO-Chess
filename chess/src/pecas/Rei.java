7db3889cc2f73991918d25c046d90cef5d160f51
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-06-18 12:42:02 -0300

    Adição da peça rei

---
+++ chess/src/pecas/Rei.java
@@ -0,0 +1,30 @@
+package pecas;
+
+public class Rei extends Peca{
+    public Rei(String cor){
+        super(cor);
+    }
+
+    public char getTipo(){
+        return 'K';
+    }
+
+    public String desenho(){
+        return "K";
+    }
+
+    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD){
+        return ((linhaO == linhaD && Math.abs(colunaO - colunaD) == 1)
+                || (colunaO == colunaD && Math.abs(linhaO - linhaD) == 1)
+                || (Math.abs(linhaO - linhaD) == 1) && (Math.abs(colunaO - colunaD) == 1));
+    }
+
+    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD){
+        String retorno = "";
+
+        if(!movimentoValido(linhaO, colunaO, linhaD, colunaD))
+            return retorno;
+
+        return retorno + linhaO + colunaO + linhaD + colunaD;
+    }
+}
