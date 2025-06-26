e83115acda0488cc5b5bbda5b4a034809cc2e630
Otávio Inácio de Oliveira <otaviogame25@gmail.com>
2025-06-26 20:23:17 -0300

    Criei classe Caminho

--- chess/src/tabuleiro/Caminho.java
+++ chess/src/tabuleiro/Caminho.java
@@ -1,1 +1,31 @@
+package tabuleiro;
 
+public class Caminho {
+    private Casa casaInicial;
+    private Casa casaFinal;
+
+    public Caminho(Casa casaInicial, Casa casaFinal){
+        this.casaInicial = casaInicial;
+        this.casaFinal = casaFinal;
+    }
+
+    public boolean estaLivre(Tabuleiro tabuleiro) {
+        String caminho = casaInicial.getPeca().caminho(casaInicial.getLinha(), casaInicial.getColuna(), casaFinal.getLinha(), casaFinal.getColuna());
+        int tamanho = caminho.length();
+
+        for(int i = 0; i < tamanho ; i+=2){
+            if(tabuleiro.casaOcupada(caminho.charAt(i), caminho.charAt(i + 1))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Casa casaInicial() {
+        return casaInicial;
+    }
+
+    public Casa casaFinal(){
+        return casaFinal;
+    }
+}
