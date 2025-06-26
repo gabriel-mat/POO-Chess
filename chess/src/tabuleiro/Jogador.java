afb6a3c30c59a633d5369692ba94c57f65b39dca
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-06-26 20:34:43 -0300

    Implementação base do jogador para o uso nas demais classes

---
+++ chess/src/tabuleiro/Jogador.java
@@ -0,0 +1,13 @@
+package tabuleiro;
+
+public class Jogador {
+    private final String cor;
+
+    public Jogador(String cor){
+        this.cor = cor;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+}
