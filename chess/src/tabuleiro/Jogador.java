9026a0d3b51d7f1397bf876d14427a60e2b943a0
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-06-26 21:26:57 -0300

    Implementação base da classe Jogador

--- chess/src/tabuleiro/Jogador.java
+++ chess/src/tabuleiro/Jogador.java
@@ -1,13 +1,71 @@
 package tabuleiro;
 
-public class Jogador {
-    private final String cor;
+import java.util.Scanner;
+import pecas.Peca;
 
-    public Jogador(String cor){
+public class Jogador {
+    private int numPecas;
+    private int numCapturadas;
+    private final String cor, nome;
+    private Peca[] pecas = new Peca[16];
+    private char[] capturadas = new char[16];
+
+    public Jogador(String cor, String nome){
         this.cor = cor;
+        this.nome = nome;
+        numCapturadas = numPecas = 0;
+
+        for(int i = 0; i < 16; i++) {
+            pecas[i] = null;
+            capturadas[i] = '\0';
+        }
     }
 
     public String getCor(){
         return cor;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String informaJogada(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira sua jogada (ou 'parar'):");
+        return scanner.nextLine();
+    }
+
+    public boolean adicionarPeca(Peca p){
+        if(numPecas >= 16)
+            return false;
+
+        pecas[numPecas++] = p;
+        return true;
+    }
+
+    public boolean adicionarCapturada(char tipo){
+        if(numCapturadas >= 16)
+            return false;
+
+        capturadas[numCapturadas++] = tipo;
+        return true;
+    }
+
+    public int getNumPecas(){
+        return numPecas;
+    }
+
+    public int getNumCapturadas(){
+        return numCapturadas;
+    }
+
+    public String pecasCapturadas(){
+        String s = "";
+
+        for(int i = 0; i < numCapturadas; i++)
+            s += capturadas[i] + " ";
+
+        return s.substring(0, s.length() - 1);
     }
 }
