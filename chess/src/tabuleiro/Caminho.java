b98ca28f4df8b4b6c4731919a8612987eb0488fe
letícia <lele.ramos.07@gmail.com>
2025-07-19 12:47:09 -0300

    Altera armazenamento do caminho de casas

--- chess/src/tabuleiro/Caminho.java
+++ chess/src/tabuleiro/Caminho.java
@@ -1,32 +1,36 @@
 package tabuleiro;
 
+import java.util.ArrayList;
+
 public class Caminho {
     private Tabuleiro tabuleiro;
-    private Casa casaInicial, casaFinal;
+    private ArrayList<Casa> casas;
 
     public Caminho(Casa casaInicial, Casa casaFinal, Tabuleiro tabuleiro){
-        this.casaInicial = casaInicial;
-        this.casaFinal = casaFinal;
         this.tabuleiro = tabuleiro;
+        casas = new ArrayList<>();
+        construirCaminho(casaInicial, casaFinal);
+    }
+
+    private void construirCaminho(Casa inicio, Casa fim) {
+        String c = inicio.getPeca().caminho(inicio.getLinha(), inicio.getColuna(), fim.getLinha(), fim.getColuna());
+        for (int i = 0; i < c.length(); i+=2)
+            casas.add(tabuleiro.getCasa(c.charAt(i) - '0', c.charAt(i + 1)));
     }
 
     public boolean estaLivre() {
-        String caminho = casaInicial.getPeca().caminho(casaInicial.getLinha(), casaInicial.getColuna(), casaFinal.getLinha(), casaFinal.getColuna());
-        int tamanho = caminho.length();
-
-        for(int i = 2; i < tamanho - 2 ; i+=2){
-            if(tabuleiro.casaOcupada(caminho.charAt(i) - '0', caminho.charAt(i + 1))){
+        for(int i = 1; i < casas.size()-1; i++)
+            if(!casas.get(i).casaVazia())
                 return false;
-            }
-        }
+
         return true;
     }
 
     public Casa casaInicial() {
-        return casaInicial;
+        return casas.getFirst();
     }
 
     public Casa casaFinal() {
-        return casaFinal;
+        return casas.getLast();
     }
 }
