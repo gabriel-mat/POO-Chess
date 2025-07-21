3ada7a95fc17e8e2cb8cab30de22efa9c2925cf7
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-07-21 12:45:08 -0300

    Primeiro movimento pode ser maior

--- chess/src/pecas/Peao.java
+++ chess/src/pecas/Peao.java
@@ -18,10 +18,12 @@ public class Peao extends Peca{
     public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) {
         if (getCor().equals("Branco"))
             return ((colunaO == colunaD) && (linhaD == linhaO + 1)) ||
-                    ((Math.abs(colunaD - colunaO) == 1) && (linhaD == linhaO + 1));
+                    ((Math.abs(colunaD - colunaO) == 1) && (linhaD == linhaO + 1) ||
+                            linhaO == 2 && colunaO == colunaD && linhaD == linhaO + 2);
 
         return ((colunaO == colunaD) && (linhaD == linhaO - 1)) ||
-                ((Math.abs(colunaD - colunaO) == 1) && (linhaD == linhaO - 1));
+                ((Math.abs(colunaD - colunaO) == 1) && (linhaD == linhaO - 1) ||
+                            linhaO == 7 && colunaO == colunaD && linhaD == linhaO - 2);
 
     }
 
